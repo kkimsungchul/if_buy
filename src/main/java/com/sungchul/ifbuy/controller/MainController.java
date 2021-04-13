899da13af0ae88db83b7af97a6ffc6ddd97881a3
securus02 <securusgit2@gmail.com>
2021-04-13 22:07:00 +0900

    2021-04-13 김성철
    레스트템플릿 테스트
    포트번호 변경
    thymeleaf 라이브러리 추가

--- src/main/java/com/sungchul/ifbuy/controller/MainController.java
+++ src/main/java/com/sungchul/ifbuy/controller/MainController.java
@@ -1,12 +1,27 @@
 package com.sungchul.ifbuy.controller;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sungchul.ifbuy.vo.VO;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class MainController {
@@ -42,5 +57,163 @@ public class MainController {
 
 
 
+
+    @GetMapping("temperature")
+    public String temperature(){
+        HashMap<String, Object> result = new HashMap<String, Object>();
+
+        String jsonInString = "";
+
+        try {
+
+            HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+            factory.setConnectTimeout(5000); //타임아웃 설정 5초
+            factory.setReadTimeout(5000);//타임아웃 설정 5초
+            RestTemplate restTemplate = new RestTemplate(factory);
+
+            HttpHeaders header = new HttpHeaders();
+            HttpEntity<?> entity = new HttpEntity<>(header);
+
+            String url = "https://api.hangang.msub.kr/";
+
+
+            UriComponents uri = UriComponentsBuilder.fromHttpUrl(url).build();
+
+            //이 한줄의 코드로 API를 호출해 MAP타입으로 전달 받는다.
+            ResponseEntity<Map> resultMap = restTemplate.exchange(uri.toString(), HttpMethod.GET, entity, Map.class);
+            result.put("statusCode", resultMap.getStatusCodeValue()); //http status code를 확인
+            result.put("header", resultMap.getHeaders()); //헤더 정보 확인
+            result.put("body", resultMap.getBody()); //실제 데이터 정보 확인
+
+            //데이터를 제대로 전달 받았는지 확인 string형태로 파싱해줌
+            ObjectMapper mapper = new ObjectMapper();
+            jsonInString = mapper.writeValueAsString(resultMap.getBody());
+
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            result.put("statusCode", e.getRawStatusCode());
+            result.put("body"  , e.getStatusText());
+            System.out.println("dfdfdfdf");
+            System.out.println(e.toString());
+
+        } catch (Exception e) {
+            result.put("statusCode", "999");
+            result.put("body"  , "excpetion오류");
+            System.out.println(e.toString());
+        }
+        return jsonInString;
+    }
+
+    @GetMapping("coin")
+    public String coin(){
+        HashMap<String, Object> result = new HashMap<String, Object>();
+
+        String jsonInString = "";
+
+        try {
+
+            HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+            factory.setConnectTimeout(5000); //타임아웃 설정 5초
+            factory.setReadTimeout(5000);//타임아웃 설정 5초
+            RestTemplate restTemplate = new RestTemplate(factory);
+
+            HttpHeaders header = new HttpHeaders();
+            HttpEntity<?> entity = new HttpEntity<>(header);
+
+            //String url = "https://api.hangang.msub.kr/";
+            String url = "https://api.upbit.com/v1/market/all?isDetails=false";
+
+            UriComponents uri = UriComponentsBuilder.fromHttpUrl(url).build();
+
+            //이 한줄의 코드로 API를 호출해 MAP타입으로 전달 받는다.
+            ResponseEntity<Object> resultMap = restTemplate.exchange(uri.toString(), HttpMethod.GET, entity, Object.class);
+            result.put("statusCode", resultMap.getStatusCodeValue()); //http status code를 확인
+            result.put("header", resultMap.getHeaders()); //헤더 정보 확인
+            result.put("body", resultMap.getBody()); //실제 데이터 정보 확인
+
+            //데이터를 제대로 전달 받았는지 확인 string형태로 파싱해줌
+            ObjectMapper mapper = new ObjectMapper();
+            jsonInString = mapper.writeValueAsString(resultMap.getBody());
+
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            result.put("statusCode", e.getRawStatusCode());
+            result.put("body"  , e.getStatusText());
+            System.out.println("dfdfdfdf");
+            System.out.println(e.toString());
+
+        } catch (Exception e) {
+            result.put("statusCode", "999");
+            result.put("body"  , "excpetion오류");
+            System.out.println(e.toString());
+        }
+        return jsonInString;
+    }
+
+
+
+    @GetMapping("coinValue")
+    public String coinValue(){
+        HashMap<String, Object> result = new HashMap<String, Object>();
+
+        String jsonInString = "";
+
+        try {
+
+            HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+            factory.setConnectTimeout(5000); //타임아웃 설정 5초
+            factory.setReadTimeout(5000);//타임아웃 설정 5초
+            RestTemplate restTemplate = new RestTemplate(factory);
+
+            HttpHeaders header = new HttpHeaders();
+
+            HttpEntity<?> entity = new HttpEntity<>(header);
+
+            //String url = "https://api.hangang.msub.kr/";
+            String url = "https://api.upbit.com/v1/candles/days?market=KRW-BTC&count=1";
+
+            UriComponents uri = UriComponentsBuilder.fromHttpUrl(url).build();
+
+            //이 한줄의 코드로 API를 호출해 MAP타입으로 전달 받는다.
+            ResponseEntity<Object> resultMap = restTemplate.exchange(uri.toString(), HttpMethod.GET, entity, Object.class);
+            result.put("statusCode", resultMap.getStatusCodeValue()); //http status code를 확인
+            result.put("header", resultMap.getHeaders()); //헤더 정보 확인
+            result.put("body", resultMap.getBody()); //실제 데이터 정보 확인
+
+            //데이터를 제대로 전달 받았는지 확인 string형태로 파싱해줌
+            ObjectMapper mapper = new ObjectMapper();
+            jsonInString = mapper.writeValueAsString(resultMap.getBody());
+
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            result.put("statusCode", e.getRawStatusCode());
+            result.put("body"  , e.getStatusText());
+            System.out.println("dfdfdfdf");
+            System.out.println(e.toString());
+
+        } catch (Exception e) {
+            result.put("statusCode", "999");
+            result.put("body"  , "excpetion오류");
+            System.out.println(e.toString());
+        }
+        return jsonInString;
+    }
+
+
+
+    //코인정보
+    @GetMapping("test1")
+    public Object test1(){
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setReadTimeout(5000); // 읽기시간초과, ms
+        factory.setConnectTimeout(3000); // 연결시간초과, ms
+        HttpClient httpClient = HttpClientBuilder.create().setMaxConnTotal(100) // connection pool 적용
+                .setMaxConnPerRoute(5) // connection pool 적용
+                .build();
+        factory.setHttpClient(httpClient); // 동기실행에 사용될 HttpClient 세팅
+        RestTemplate restTemplate = new RestTemplate(factory);
+        String url = "https://api.upbit.com/v1/candles/days?count=1"; // 예제니까 애초에 때려박음..
+        Object obj = restTemplate.getForObject(url , Object.class);
+        System.out.println(obj);
+
+        return obj;
+    }
 
 }

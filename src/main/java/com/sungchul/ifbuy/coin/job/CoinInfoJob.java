521436464b5bdd62ea22641a6182b79998222c09
kkimsungchul <kimsc1218@gmail.com>
2021-04-26 00:59:45 +0900

    2021-04-25 김성철
    # JAVA
    Junit 추가
    스케줄러 방싱 변경(cron)
    최근 120일의 코인 가격을 가져오는 초기화 가능 추가
    
    # HTML
    .없음
    
    # JS
    .없음
    
    # DB
    데이터베이스 생성 쿼리문 수정 (코인가격을 월별, 일별로 나눠 담도록 분리)
    
    # Gradle
    Junit 라이브러리 추가

--- src/main/java/com/sungchul/ifbuy/coin/job/CoinInfoJob.java
+++ src/main/java/com/sungchul/ifbuy/coin/job/CoinInfoJob.java
@@ -16,29 +16,31 @@ public class CoinInfoJob {
 
 
 
-//
-//
-//    @Scheduled(cron ="0 1 9 * * *")
-//    public void getUpbitCoinInfoDay(){
-//        log.info("# getUpbitCoinInfoDay Scheduled Start ! ");
-//        String period = "days";
-//        CoinInfoThread coinInfoThread = new CoinInfoThread();
-//        coinInfoThread.run(period);
-//    }
-//
-//    @Scheduled(cron ="* */1 * * * *")
-//    public void getUpbitCoinInfoMinute(){
-//        log.info("# Scheduled Scheduled Start ! ");
-//        String period = "minutes";
-//        CoinInfoThread coinInfoThread = new CoinInfoThread();
-//        coinInfoThread.run(period);
-//    }
-//
-//
-//    @Scheduled(cron ="0 0 9 * * *")
-//    public void deleteCoinInfoMinute(){
-//        log.info("# CoinInfoMinute Scheduled Start ! ");
-//        coinService.deleteCoinInfoMinute();
-//
-//    }
+
+
+    //아침 9시 1분마다 전날 코인의 가격 가져오기
+    @Scheduled(cron ="0 1 9 * * *")
+    public void getUpbitCoinInfoDay(){
+        log.info("# getUpbitCoinInfoDay Scheduled Start ! ");
+        String period = "days";
+        CoinInfoThread coinInfoThread = new CoinInfoThread();
+        coinInfoThread.run(period);
+    }
+
+    //매분마다 코인가격정보 가져오기
+    @Scheduled(cron ="* */1 * * * *")
+    public void getUpbitCoinInfoMinute(){
+        log.info("# Scheduled Scheduled Start ! ");
+        String period = "minutes";
+        CoinInfoThread coinInfoThread = new CoinInfoThread();
+        coinInfoThread.run(period);
+    }
+
+    //아침9시에 1분단위로 저장한 코인가격 삭제
+    @Scheduled(cron ="0 0 9 * * *")
+    public void deleteCoinInfoMinute(){
+        log.info("# CoinInfoMinute Scheduled Start ! ");
+        coinService.deleteCoinInfoMinute();
+
+    }
 }
